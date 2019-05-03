b06815c651274c169d112c5cf2bec536982215d9
liuliaixue <liuliaixue@qq.com>
2019-05-03 17:14:39 +0800

    ui

--- src/basic/01_string_int_float.go
+++ src/basic/01_string_int_float.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-func typeConerter() {
+func typeConverter() {
 	a, b, c, d, e := "3", 3, int64(3), 3.0, "3.14"
 
 	fmt.Printf("%T:%v %T:%v %T:%v %T:%v ,%T:%v\n", a, a, b, b, c, c, d, d, e, e)
